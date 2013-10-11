ce46704d79257277f585d57fb3dd29583c852cad
Marcel Hoyer <m.hoyer@cid.biz>
2013-10-11 15:16:59 +0200

    adds AgUnitOptionsPage for Resharper 8.0 (to toggle ShowBrowserWindow option for StatLight)

---
+++ src/AgUnit.Runner.Resharper80.OptionsDialog/AgUnitOptionsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Application.Settings;
+using JetBrains.DataFlow;
+using JetBrains.ReSharper.UnitTestExplorer.Options;
+using JetBrains.UI.Controls;
+using JetBrains.UI.CrossFramework;
+using JetBrains.UI.Options;
+using JetBrains.UI.Resources;
+
+namespace AgUnit.Runner.Resharper80.OptionsDialog
+{
+    [OptionsPage(PID, "AgUnit", typeof(OptionsThemedIcons.Options), ParentId = UnitTestingPageBase.PID)]
+    public partial class AgUnitOptionsPage : IOptionsPage
+    {
+        public const string PID = "AgUnitOptionsPage";
+
+        public AgUnitOptionsPage(Lifetime lifetime, OptionsSettingsSmartContext settings)
+        {
+            InitializeComponent();
+            settings.SetBinding(
+                lifetime, 
+                (AgUnitSettings s) => s.IsBrowserWindowEnabled, 
+                isBrowserWindowEnabledCheckBox,
+                CheckBoxDisabledNoCheck2.IsCheckedLogicallyDependencyProperty);
+        }
+
+        public bool OnOk()
+        {
+            return true;
+        }
+
+        public bool ValidatePage()
+        {
+            return true;
+        }
+
+        public EitherControl Control
+        {
+            get { return this; }
+        }
+
+        public string Id { get; private set; }
+    }
+}
