263f0ea397134fd7ea251ecd5a420c49952a342f
Marcel Hoyer <m.hoyer@cid.biz>
2013-10-11 15:19:24 +0200

    adds AgUnitSettings to Resharper80 project

---
+++ src/AgUnit.Runner.Resharper80/AgUnitSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Application.Settings;
+
+namespace AgUnit.Runner.Resharper80
+{
+    [SettingsKey(typeof(EnvironmentSettings), "AgUnitSettings")]
+    public class AgUnitSettings
+    {
+        [SettingsEntry(false, "Show Browser Window")]
+        public bool IsBrowserWindowEnabled { get; set; }
+    }
+}
